0527eeb3917f6415daf85451b94685a21479019d
skrochsuzana <suzanaskroch@gmail.com>
2017-04-09 00:08:16 -0300

    Criação da tela "Criar Cadastro" Navegação entre Tela de logn e cadastro

---
+++ app/src/main/java/com/example/rafaelmatucheski/street2/Criar_Cadastro.java
@@ -0,0 +1,21 @@
+package com.example.rafaelmatucheski.street2;
+
+        import android.app.Activity;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+
+        import org.w3c.dom.Text;
+
+/**
+ * Created by Rafael Matucheski on 08/04/2017.
+ */
+
+public class Criar_Cadastro extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.criar_cadastro);
+    }
+}
--- app/src/main/java/com/example/rafaelmatucheski/street2/MainActivity.java
+++ app/src/main/java/com/example/rafaelmatucheski/street2/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.rafaelmatucheski.street2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,8 +11,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-
+    public void telaCriarCadastro(View v)
+    {
+        Intent tela = new Intent(this, Criar_Cadastro.class );
+        startActivity(tela);
     }
 
 }
